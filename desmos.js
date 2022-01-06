10b55c3ff4aafdfedae084d7e4a2ef78c497328a
Litten 1up <seant9140@gmail.com>
2022-01-06 12:31:34 -0600

    Update desmos.js

--- desmos.js
+++ desmos.js
@@ -1,5 +1,5 @@
 let element = document.createElement('div');
-element.innerHTML = "<div id='calculator'></div>;
+element.innerHTML = "<div id='calculator'></div>";
 document.body.appendChild(div);
 var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt);
